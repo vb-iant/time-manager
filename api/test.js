2f2f64b0963904ee95f3c99abc73c130724d376d
vb-iant <ian@velocity-b.com>
2026-07-03 09:13:09 +0100

    Add test endpoint

---
+++ api/test.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  
+  return res.json({
+    method: req.method,
+    body: req.body,
+    bodyType: typeof req.body,
+    contentType: req.headers['content-type'],
+    hasId: req.body ? !!req.body.id : false,
+  });
+}
